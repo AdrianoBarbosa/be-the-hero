30018fcf306d9db59c56c0e0d58c21a723e54e72
Adriano O. Barbosa <adriano.barbosa90@gmail.com>
2026-09-13 16:11:20 -0300

    build(backend): atualiza dependências e resolve vulnerabilidades críticas

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,59 +1,27 @@
-// Update with your config settings.
+require('dotenv').config({ quiet: true });
+
+const migrations = {
+    directory: './src/database/migrations'
+};
 
 module.exports = {
 
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './src/database/db.sqlite'
+    development: {
+        client: 'better-sqlite3',
+        connection: {
+            filename: process.env.DB_FILENAME || './src/database/db.sqlite'
+        },
+        migrations,
+        useNullAsDefault: true,
     },
-    migrations: {
-      directory: './src/database/migrations'
-    },
-    useNullAsDefault: true,
-  },
 
-  test: {
-    client: 'sqlite3',
-    connection: {
-      filename: './src/database/test.sqlite'
+    test: {
+        client: 'better-sqlite3',
+        connection: {
+            filename: ':memory:'
+        },
+        migrations,
+        useNullAsDefault: true,
     },
-    migrations: {
-      directory: './src/database/migrations'
-    },
-    useNullAsDefault: true,
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
 
 };
